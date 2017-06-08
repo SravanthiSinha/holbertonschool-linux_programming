c757952c1de7c138ab208c106f16f0848160eb0f
sravanthisinha <sravani.sinha@gmail.com>
2017-06-08 14:32:04 -0700

    fix

--- 0x00-c_dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x00-c_dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -c -Wall -Werror -fpic *.c
-gcc -shared -o liball.so *.o
+gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
