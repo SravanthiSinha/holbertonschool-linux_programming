150d933e9850ffe9e27b02e609d953621ff21ee6
sravanthisinha <sravani.sinha@gmail.com>
2017-11-09 16:29:13 -0800

    rmoving local variable

--- 0x09-libasm/100-putc.asm
+++ 0x09-libasm/100-putc.asm
@@ -13,29 +13,22 @@ asm_putc:
 	mov rbp, rsp
 				; Save registers that are gonna be used in this
 	push rsi		; procedure, in case they were used before
-	push rdi
-	push rdx
 	push rcx
+	push rdx
+	push rdi
 	;;prologue end
 
-	add rsp, -1		; Increase the stack by 1 byte to store a char
-	mov [rsp], rdi		; Store the ascii value of input char c
-
 	; Setup 'write' syscall
-	mov rax, 1		; Write syscall
-	mov rdi, 1		; write to stdout
-	mov rdx, 1		; Write 1 byte
-	mov rsi, rsp		; Address of the character to be printed (Address of the stack)
-
+	mov eax, 1		; Write syscall
+	mov edx, 1		; Write 1 byte
+	lea rsi, [rsp]		; Address of the character to be printed (Address of the stack)
+	mov edi, 1		; write to stdout
 	syscall
-end:
-	add rsp, 1		; Discard our local variable
-	mov rax, 0x1
 
 	;; epilogue
-	pop rcx			; Restore used registers
+	pop rdi			; Restore used registers
 	pop rdx
-	pop rdi
+	pop rcx
 	pop rsi
 	mov rsp, rbp		; Restore previous stack frame
 	pop rbp
