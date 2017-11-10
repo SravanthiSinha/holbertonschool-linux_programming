03e424c5a0782c67db2ddcfe1f03368c9e2eb7f4
sravanthisinha <sravani.sinha@gmail.com>
2017-11-10 13:54:02 -0800

    fixed to reduce no of calls

--- 0x09-libasm/1000-puti.asm
+++ 0x09-libasm/1000-puti.asm
@@ -4,46 +4,6 @@ BITS 64
         section .text
 	; size_t asm_puti(int n);
         ; Prints the lowercase alphabet
-print_number:
-	;; prologue
-	push rbp                ; Setup stack frame
-	mov rbp, rsp
-				; Save registers that are gonna be used in this
-	push rdi
-	push rdx
-	;;  prologue end
-        mov rax, rdi
-        cdq
-        mov rbx, 10
-        xor rdx, rdx
-        idiv rbx
-        cmp rdi, 10 		;if(n >= 10)
-        jge recur
-        inc rsi
-        xor rdi, rdi
-        mov edi, edx
-        add edi, 48
-        call asm_putc
-        jmp endprint_number
-recur:
-        mov rdi, rax
-        inc rsi
-        call print_number
-        xor rdi, rdi
-        mov edi, edx
-        add edi, 48
-        call asm_putc
-        mov rdi, rax
-endprint_number:
-        ;; epilogue
-        pop rdx 		; Restore used registers
-        pop rdi
-        mov rsp, rbp
-        pop rbp
-        ;; epilogue end
-
-        ret
-
 asm_puti:
 	;; prologue
         push rbp                ; Setup stack frame
@@ -53,23 +13,54 @@ asm_puti:
         push rdi
         push rdx
         push r13
+	push r14
+	push r8
 	;;  prologue end
 
         mov rsi, 0  		;size_t counter = 0
+	xor r13, r13
         mov r13, rdi		;long no = n
         cmp r13d, 0 		;if(no<0)
-        jns end
-        movsx r13, edi
-        neg r13
-        inc rsi
-        mov rdi, 45
-        call asm_putc
-end:
-	mov rdi, r13
-	call print_number 	;print_number(no, &counter);
-	mov rax, rsi
+        js negative
+	mov rax, r13
+	jmp print_number
 
+	negative:
+		mov r8, 1
+		movsx r13, edi
+		neg r13
+		inc rsi
+		mov rdi, 45
+		call asm_putc
+		mov rax, r13
+	print_number:
+		mov r13, rax
+		inc rsi
+		cdq
+	        mov rbx, 10
+	        xor rdx, rdx
+	        idiv rbx
+		push rdx
+	        cmp r13, 10 		;if(n >= 10)
+	        jge print_number
+		mov r14, rsi
+		cmp r8, 1
+		jne loop
+		dec r14
+	loop:
+		cmp r14, byte 0
+		je end
+		pop rdi
+	        add rdi, 48
+	        call asm_putc
+		dec r14
+		jmp loop
+	end:
+		mov rax, rsi
+	
 	;; epilogue
+	pop r8
+	pop r14
 	pop r13
 	pop rdx ; Restore used registers
 	pop rdi
