de47ef6414850d4f6bc80de2af77a05610a2e7da
sravanthisinha <sravani.sinha@gmail.com>
2017-12-17 15:37:34 -0800

    re write memcpy

--- 0x09-libasm/5-memcpy.asm
+++ 0x09-libasm/5-memcpy.asm
@@ -7,35 +7,8 @@ BITS 64
 	; int asm_memcpy(void *dest, const void *src, size_t n);
 	; copies n bytes from memory area src to memory area dest
 asm_memcpy:
-	;;prologue
-	push rbp		; Set up stack frame
-	mov rbp, rsp
-	push rcx
-	push r15
-	;;prologue end
-
-	mov r15b, 0		; variable c
-	mov rcx, rdx		; counter i
-
-;loop_str - while loop
-loop_str:
-	cmp [rdi], byte 0 	; (!dest)
-	je loopend
-	mov r15b, [rsi] 	; c = *src
-	mov [rdi], r15b         ; *dest = c
-	add rdi, 1		; Increment the first string *dest++;
-	add rsi, 1		; Increment the second string *src++;
-	cmp rcx,  0		; (count > 0)
-	jle loopend
-	cmp [rsi], byte 0 	; (!src)
-	je loopend
-	sub rcx, 1;
-	jmp loop_str
-loopend:
-	;;epilogue
-	pop r15
-	pop rcx
-	mov rsp, rbp		; Restore previous stack frame
-	pop rbp
-	;;epilogue end
-	ret			; Return from procedure
+	mov rax, rdi		; Copy the pointer to @dest string to be returned
+	mov ecx, edx		; Copy the length @n to the counter
+	cld			; Clear the direction flag, DL
+	rep movsb		; Copy @n bytes from ESI (source) to EDI (destination)
+	ret			; Return
