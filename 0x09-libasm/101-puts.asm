307c79cb0e34cd2d7f6da25b7f0c8fa9dbbec627
sravanthisinha <sravani.sinha@gmail.com>
2017-12-17 15:17:36 -0800

    fix indentation

--- 0x09-libasm/101-puts.asm
+++ 0x09-libasm/101-puts.asm
@@ -11,18 +11,16 @@ asm_puts:
 	;; prologue
         push rbp                ; Setup stack frame
         mov rbp, rsp
-                                ; Save registers that are gonna be used in this
-        push rsi                ; procedure, in case they were used before
-        push rdi
+        push rsi		; Save registers that are gonna be used in this
+        push rdi                ; procedure, in case they were used before
         push rdx
         push r14
 	;;  prologue end
 	
         call asm_strlen
-	
         mov r14, rax
         mov r13, rdi
-	
+
 loop_str:
         cmp [r13], byte 0
         je end
@@ -31,8 +29,7 @@ loop_str:
         mov rax, 1              ; Write syscall
 	mov rdi, 1              ; write to stdout
 	mov rdx, 1              ; Write 1 byte
-	mov rsi, r13            ; Address of the character to be printed (Address of the stack)
-
+	mov rsi, r13            ; Address of the character to be printed
 	syscall
 	add r13, 1
 	jmp loop_str
@@ -47,5 +44,5 @@ end:
 	mov rsp, rbp
 	pop rbp
 	;; epilogue end
-	
+
 	ret
